f881e2b0318daa70328d0149c8460785e987736d
Alex Ghiurau <alex.ghiurau@gmail.com>
2023-10-19 09:25:59 +0300

    remove files, and add in proper folder

--- 4 -  React Introduction II/src/App.jsx
+++ 4 -  React Introduction II/src/App.jsx
@@ -3,33 +3,71 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const CarPhoto = (props) => {
+  const { photo } = props
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <img
+    style={{
+      objectFit: 'contain'
+    }}
+    height={150}
+    src={photo}>
+
+  </img>
+}
+
+const Title = (props) => {
+  const { title } = props
+  return <h2>{title}</h2>
+}
+
+const Description = (props) => {
+  const { text } = props
+  return <p style={{ color: '#ccc' }}>{text}</p>
+}
+
+const CarTile = (props) => {
+  const { description, photo, brand } = props
+
+  return <div className={'container'}>
+    <CarPhoto photo={photo} />
+    <Title title={brand} />
+    <Description text={description} />
+
+  </div>
+}
+
+
+const cars = [
+  {
+    brand: "Toyota",
+    description: "The Toyota Camry is a reliable and fuel-efficient sedan.",
+    photo: "https://scene7.toyota.eu/is/image/toyotaeurope/toyota-camry-2021-gallery-04-full_tcm-10-2203478?wid=1280&fit=fit,1&ts=1651157703495&resMode=sharp2&op_usm=1.75,0.3,2,0"
+  },
+  {
+    brand: "Honda",
+    description: "The Honda Civic is known for its sporty design and great handling.",
+    photo: "https://honda.ro/images/cars.jpg"
+  },
+  {
+    brand: "Ford",
+    description: "The Ford F-150 is a popular pickup truck with excellent towing capacity.",
+    photo: "https://www.ford.ro/content/dam/guxeu/nextgen/cx740s/reveal/ford-eu-cx740s-homepage-NextGen_Desktop-21x9-2160x925-bb.jpg.renditions.original.png"
+  },
+];
+
+function App() {
+  return <div>
+    {cars.map((car, index ) => {
+      return <CarTile
+        key={index}
+        brand={car.brand}
+        description={car.description}
+        photo={car.photo}
+      />
+    })}
+  </div>
+
 }
 
 export default App
