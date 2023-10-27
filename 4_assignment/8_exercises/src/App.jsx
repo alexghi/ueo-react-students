147055981f3a2fae98eaea5f9e16fd6eec3e56f4
Turcu11 <iosif.turcu@emanuel.ro>
2023-10-27 21:35:31 +0300

    fixed the key problem for the array

--- 4_assignment/8_exercises/src/App.jsx
+++ 4_assignment/8_exercises/src/App.jsx
@@ -1,44 +1,43 @@
 import './App.css'
+const data = [
+  {
+    id: 1,
+    setup: "What's the best thing about a Boolean?",
+    punchline: "Even if you're wrong, you're only off by a bit"
+  },
+  {
+    id: 2,
+    setup: "Why do programmers wear glasses?",
+    punchline: "Because they need to C#"
+  }];
 
+const DataCard = (props) => {
+  const { jokeSetup, jokePunchline, keyProp } = props;
+  console.log(keyProp);
+  return (
+    <div key={keyProp} className='jokeCard'>
+      <h2>{jokeSetup}</h2>
+      <p>{jokePunchline}</p>
+    </div>
+  )
+}
 function App() {
-  const data = [
-    {
-      id: 1,
-      setup: "What's the best thing about a Boolean?",
-      punchline: "Even if you're wrong, you're only off by a bit"
-    },
-    {
-      id: 2,
-      setup: "Why do programmers wear glasses?",
-      punchline: "Because they need to C#"
-    }
-  ]
-
-  const DataCard = (props) => {
-    const {jokeSetup, jokePunchline } = props
-    return <>
-      <div className='jokeCard'>
-          <h2>{jokeSetup}</h2>
-          <p>{jokePunchline}</p>
-      </div>
-    </>
-  }
 
   return (
     <>
       <div className="card">
-        {data.map((joke, index)=>{
-          return<>
-            <DataCard 
-            key={index}
-            jokeSetup={joke.setup}
-            jokePunchline={joke.punchline}
+        {data.map((joke, index) => (
+          <div key={'key' + joke.id}>
+            <DataCard
+              keyProp={index}
+              jokeSetup={joke.setup}
+              jokePunchline={joke.punchline}
             />
-          </>
-        })}
+          </div>
+        ))}
       </div>
     </>
   )
 }
 
-export default App
+export default App;
